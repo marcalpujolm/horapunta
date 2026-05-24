237cea1e01dc03495497f876e305040ce4e3f1e9
marsipm <marcalpujol2000@gmail.com>
2026-05-24 16:33:54 +0200

    Revert "anunci cinematografic Felicitats - 28s, 4 beats, pur CSS Remotion"
    This reverts commit 95576f31b404ff3c223bd2927322728c3963f8ac.

--- horapunta-reel/src/Root.tsx
+++ horapunta-reel/src/Root.tsx
@@ -1,11 +1,10 @@
 import "./index.css";
 import { Composition } from "remotion";
-import { HoraPuntaPromo }   from "./HoraPuntaPromo";
-import { SAdFelicitats }    from "./scenes/SAdFelicitats";
+import { HoraPuntaPromo } from "./HoraPuntaPromo";
 
 export const RemotionRoot: React.FC = () => (
   <>
-    {/* 52s CSS promo — vertical */}
+    {/* 60s CSS 3D promotional video — vertical */}
     <Composition
       id="HoraPuntaPromo"
       component={HoraPuntaPromo}
@@ -15,7 +14,7 @@ export const RemotionRoot: React.FC = () => (
       height={1920}
     />
 
-    {/* 52s CSS promo — horizontal */}
+    {/* 60s CSS 3D promotional video — horizontal */}
     <Composition
       id="HoraPuntaPromo-H"
       component={HoraPuntaPromo}
@@ -25,24 +24,5 @@ export const RemotionRoot: React.FC = () => (
       height={1080}
     />
 
-    {/* 28s AD "Felicitats" — vertical (Stories / Reels) */}
-    <Composition
-      id="Ad-Felicitats"
-      component={SAdFelicitats}
-      durationInFrames={840}
-      fps={30}
-      width={1080}
-      height={1920}
-    />
-
-    {/* 28s AD "Felicitats" — horizontal (YouTube / LinkedIn) */}
-    <Composition
-      id="Ad-Felicitats-H"
-      component={SAdFelicitats}
-      durationInFrames={840}
-      fps={30}
-      width={1920}
-      height={1080}
-    />
   </>
 );
